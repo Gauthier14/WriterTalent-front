f9dc05c65fea0d0d23ea701368d39e0af1d3a72c
Chrstn67 <chrstn.hmbrt67@outlook.com>
2023-04-17 18:55:47 +0200

    correction SCSS

--- src/components/Authors/Authors.js
+++ src/components/Authors/Authors.js
@@ -11,6 +11,46 @@ function Authors() {
           <li className="page-author page1-author"></li>
         </ul>
       </div>
+      <div className="book-author">
+        <ul>
+          <li className="cover-author"> <a href link="#">Donald Duck</a> </li>
+          <li className="page-author page3-author"></li>
+          <li className="page-author page2-author"></li>
+          <li className="page-author page1-author"></li>
+        </ul>
+      </div>
+      <div className="book-author">
+        <ul>
+          <li className="cover-author"> <a href link="#">Donald Duck</a> </li>
+          <li className="page-author page3-author"></li>
+          <li className="page-author page2-author"></li>
+          <li className="page-author page1-author"></li>
+        </ul>
+      </div>
+      <div className="book-author">
+        <ul>
+          <li className="cover-author"> <a href link="#">Donald Duck</a> </li>
+          <li className="page-author page3-author"></li>
+          <li className="page-author page2-author"></li>
+          <li className="page-author page1-author"></li>
+        </ul>
+      </div>
+      <div className="book-author">
+        <ul>
+          <li className="cover-author"> <a href link="#">Donald Duck</a> </li>
+          <li className="page-author page3-author"></li>
+          <li className="page-author page2-author"></li>
+          <li className="page-author page1-author"></li>
+        </ul>
+      </div>
+      <div className="book-author">
+        <ul>
+          <li className="cover-author"> <a href link="#">Donald Duck</a> </li>
+          <li className="page-author page3-author"></li>
+          <li className="page-author page2-author"></li>
+          <li className="page-author page1-author"></li>
+        </ul>
+      </div>
       </section>
   );
 }
