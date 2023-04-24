ae030ebcb76df2a89982fcae2e9945187e47734b
Sofiane Khenoune <modestman1986@yahoo.com>
2023-04-24 19:00:37 +0200

    continue viewer test

--- src/components/ViewerPost/ViewerPost.js
+++ src/components/ViewerPost/ViewerPost.js
@@ -1,13 +1,41 @@
 import HTMLFlipBook from "react-pageflip";
+import "./ViewerPost.scss";
 
 function ViewerPost() {
   return (
-    <HTMLFlipBook width={300} height={500} size="stretch">
-      <div className="demoPage">Page 1</div>
-      <div className="demoPage">Page 2</div>
-      <div className="demoPage">Page 3</div>
-      <div className="demoPage">Page 4</div>
-    </HTMLFlipBook>
+    <main className="viewer">
+      <HTMLFlipBook width={400} height={600} size="fixed">
+        <div className="demoPage">Page 1
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum odit earum sint minus voluptatibus dolor! Exercitationem, deleniti ad cupiditate voluptatem ab magni laboriosam velit! Aut accusantium quas architecto commodi in.
+            Tenetur laborum in qui aspernatur assumenda ratione, porro sint ipsam nobis omnis soluta veniam! Cum consequuntur recusandae placeat numquam aut sunt incidunt dolorum soluta ad nisi suscipit, itaque accusantium unde.
+            Totam dolorem, repellat sit libero omnis quod ratione beatae! Natus doloribus assumenda, sed nesciunt, optio eaque at repellat explicabo odio maxime, voluptatem accusantium illo iure commodi nam atque. Temporibus, minus.
+            Est hic error libero officia ab possimus ducimus velit fugiat, dolor enim quidem autem similique, labore laborum dignissimos voluptatum consequuntur nesciunt, quasi obcaecati neque reiciendis deserunt! Totam quae iste voluptatum.
+            In placeat hic soluta quo harum nulla perspiciatis, quis fuga error inventore similique, earum, officia reprehenderit accusantium vitae nam eligendi. Magnam officia ad error nesciunt saepe, facilis obcaecati iure magni!
+          </p>
+        </div>
+        <div className="demoPage">Page 2 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum odit earum sint minus voluptatibus dolor! Exercitationem, deleniti ad cupiditate voluptatem ab magni laboriosam velit! Aut accusantium quas architecto commodi in.
+          Tenetur laborum in qui aspernatur assumenda ratione, porro sint ipsam nobis omnis soluta veniam! Cum consequuntur recusandae placeat numquam aut sunt incidunt dolorum soluta ad nisi suscipit, itaque accusantium unde.
+          Totam dolorem, repellat sit libero omnis quod ratione beatae! Natus doloribus assumenda, sed nesciunt, optio eaque at repellat explicabo odio maxime, voluptatem accusantium illo iure commodi nam atque. Temporibus, minus.
+          Est hic error libero officia ab possimus ducimus velit fugiat, dolor enim quidem autem similique, labore laborum dignissimos voluptatum consequuntur nesciunt, quasi obcaecati neque reiciendis deserunt! Totam quae iste voluptatum.
+          In placeat hic soluta quo harum nulla perspiciatis, quis fuga error inventore similique, earum, officia reprehenderit accusantium vitae nam eligendi. Magnam officia ad error nesciunt saepe, facilis obcaecati iure magni!
+        </p>
+        </div>
+        <div className="demoPage">Page 3 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum odit earum sint minus voluptatibus dolor! Exercitationem, deleniti ad cupiditate voluptatem ab magni laboriosam velit! Aut accusantium quas architecto commodi in.
+          Tenetur laborum in qui aspernatur assumenda ratione, porro sint ipsam nobis omnis soluta veniam! Cum consequuntur recusandae placeat numquam aut sunt incidunt dolorum soluta ad nisi suscipit, itaque accusantium unde.
+          Totam dolorem, repellat sit libero omnis quod ratione beatae! Natus doloribus assumenda, sed nesciunt, optio eaque at repellat explicabo odio maxime, voluptatem accusantium illo iure commodi nam atque. Temporibus, minus.
+          Est hic error libero officia ab possimus ducimus velit fugiat, dolor enim quidem autem similique, labore laborum dignissimos voluptatum consequuntur nesciunt, quasi obcaecati neque reiciendis deserunt! Totam quae iste voluptatum.
+          In placeat hic soluta quo harum nulla perspiciatis, quis fuga error inventore similique, earum, officia reprehenderit accusantium vitae nam eligendi. Magnam officia ad error nesciunt saepe, facilis obcaecati iure magni!
+        </p>
+        </div>
+        <div className="demoPage">Page 4 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum odit earum sint minus voluptatibus dolor! Exercitationem, deleniti ad cupiditate voluptatem ab magni laboriosam velit! Aut accusantium quas architecto commodi in.
+          Tenetur laborum in qui aspernatur assumenda ratione, porro sint ipsam nobis omnis soluta veniam! Cum consequuntur recusandae placeat numquam aut sunt incidunt dolorum soluta ad nisi suscipit, itaque accusantium unde.
+          Totam dolorem, repellat sit libero omnis quod ratione beatae! Natus doloribus assumenda, sed nesciunt, optio eaque at repellat explicabo odio maxime, voluptatem accusantium illo iure commodi nam atque. Temporibus, minus.
+          Est hic error libero officia ab possimus ducimus velit fugiat, dolor enim quidem autem similique, labore laborum dignissimos voluptatum consequuntur nesciunt, quasi obcaecati neque reiciendis deserunt! Totam quae iste voluptatum.
+          In placeat hic soluta quo harum nulla perspiciatis, quis fuga error inventore similique, earum, officia reprehenderit accusantium vitae nam eligendi. Magnam officia ad error nesciunt saepe, facilis obcaecati iure magni!
+        </p>
+        </div>
+      </HTMLFlipBook>
+    </main>
   );
 }
 
