ebfe58aadbee9c1491f40c55916e9c3b48451446
Sofiane Khenoune <modestman1986@yahoo.com>
2023-04-24 20:19:00 +0200

    game over

--- src/components/ViewerPost/ViewerPost.js
+++ src/components/ViewerPost/ViewerPost.js
@@ -18,21 +18,21 @@ function ViewerPost() {
           Totam dolorem, repellat sit libero omnis quod ratione beatae! Natus doloribus assumenda, sed nesciunt, optio eaque at repellat explicabo odio maxime, voluptatem accusantium illo iure commodi nam atque. Temporibus, minus.
           Est hic error libero officia ab possimus ducimus velit fugiat, dolor enim quidem autem similique, labore laborum dignissimos voluptatum consequuntur nesciunt, quasi obcaecati neque reiciendis deserunt! Totam quae iste voluptatum.
           In placeat hic soluta quo harum nulla perspiciatis, quis fuga error inventore similique, earum, officia reprehenderit accusantium vitae nam eligendi. Magnam officia ad error nesciunt saepe, facilis obcaecati iure magni!
-        </p>
+                                         </p>
         </div>
         <div className="demoPage">Page 3 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum odit earum sint minus voluptatibus dolor! Exercitationem, deleniti ad cupiditate voluptatem ab magni laboriosam velit! Aut accusantium quas architecto commodi in.
           Tenetur laborum in qui aspernatur assumenda ratione, porro sint ipsam nobis omnis soluta veniam! Cum consequuntur recusandae placeat numquam aut sunt incidunt dolorum soluta ad nisi suscipit, itaque accusantium unde.
           Totam dolorem, repellat sit libero omnis quod ratione beatae! Natus doloribus assumenda, sed nesciunt, optio eaque at repellat explicabo odio maxime, voluptatem accusantium illo iure commodi nam atque. Temporibus, minus.
           Est hic error libero officia ab possimus ducimus velit fugiat, dolor enim quidem autem similique, labore laborum dignissimos voluptatum consequuntur nesciunt, quasi obcaecati neque reiciendis deserunt! Totam quae iste voluptatum.
           In placeat hic soluta quo harum nulla perspiciatis, quis fuga error inventore similique, earum, officia reprehenderit accusantium vitae nam eligendi. Magnam officia ad error nesciunt saepe, facilis obcaecati iure magni!
-        </p>
+                                         </p>
         </div>
         <div className="demoPage">Page 4 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum odit earum sint minus voluptatibus dolor! Exercitationem, deleniti ad cupiditate voluptatem ab magni laboriosam velit! Aut accusantium quas architecto commodi in.
           Tenetur laborum in qui aspernatur assumenda ratione, porro sint ipsam nobis omnis soluta veniam! Cum consequuntur recusandae placeat numquam aut sunt incidunt dolorum soluta ad nisi suscipit, itaque accusantium unde.
           Totam dolorem, repellat sit libero omnis quod ratione beatae! Natus doloribus assumenda, sed nesciunt, optio eaque at repellat explicabo odio maxime, voluptatem accusantium illo iure commodi nam atque. Temporibus, minus.
           Est hic error libero officia ab possimus ducimus velit fugiat, dolor enim quidem autem similique, labore laborum dignissimos voluptatum consequuntur nesciunt, quasi obcaecati neque reiciendis deserunt! Totam quae iste voluptatum.
           In placeat hic soluta quo harum nulla perspiciatis, quis fuga error inventore similique, earum, officia reprehenderit accusantium vitae nam eligendi. Magnam officia ad error nesciunt saepe, facilis obcaecati iure magni!
-        </p>
+                                         </p>
         </div>
       </HTMLFlipBook>
     </main>
