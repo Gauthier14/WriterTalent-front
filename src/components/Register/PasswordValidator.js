650b0fb2caaa37c365f8583db0a1760afb63d393
Chrstn67 <chrstn.hmbrt67@outlook.com>
2023-05-02 12:59:58 +0200

    DomPurify LoginForm bug

--- src/components/Register/PasswordValidator.js
+++ src/components/Register/PasswordValidator.js
@@ -1,27 +1,27 @@
-import React from "react";
-import PasswordValidator from "./PasswordValidator";
+// import React from "react";
+// import PasswordValidator from "./PasswordValidator";
 
-const PasswordValidator = () => {
-return (
-<form>
-<label htmlFor="password">Mot de passe:</label>
-<input type="password" id="password" name="password" />
-<div className="password-errors">
-{passwordCriteria.map((criterion) => (
-<span key={criterion.name} data-criterion={criterion.name}>
-{criterion.message}
-</span>
-))}
-</div>
+// const PasswordValidator = () => {
+// return (
+// <form>
+// <label htmlFor="password">Mot de passe:</label>
+// <input type="password" id="password" name="password" />
+// <div className="password-errors">
+// {passwordCriteria.map((criterion) => (
+// <span key={criterion.name} data-criterion={criterion.name}>
+// {criterion.message}
+// </span>
+// ))}
+// </div>
 
-  <label htmlFor="confirm-password">Confirmez le mot de passe:</label>
-  <input type="password" id="confirm-password" name="confirm-password" />
+//   <label htmlFor="confirm-password">Confirmez le mot de passe:</label>
+//   <input type="password" id="confirm-password" name="confirm-password" />
 
-  <button type="submit">Valider</button>
+//   <button type="submit">Valider</button>
 
-  <PasswordValidator />
-</form>
-);
-};
+//   <PasswordValidator />
+// </form>
+// );
+// };
 
-export default PasswordValidator;
+// export default PasswordValidator;
